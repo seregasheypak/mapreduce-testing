5ea4eceba7a28bc2ccd8b5618b893c0e1872defd
seregasheypak <serega.sheypak@gmail.com>
2013-03-23 16:03:22 +0400

    Add counter for mapper

---
+++ mapreduce-testing-mrjob/src/main/java/ru/develbureau/mrtesting/model/Counter.java
@@ -0,0 +1,11 @@
+package ru.develbureau.mrtesting.model;
+
+/**
+ * User: sergey.sheypak
+ * Date: 23.03.13
+ * Time: 16:02
+ */
+public enum Counter {
+    CORRUPTED_RECORD,
+    SERVER_ERROR_500
+}
