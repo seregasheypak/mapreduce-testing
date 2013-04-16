2a9875a0adda3749c10e5e1c4cd8895a0ae47afd
seregasheypak <serega.sheypak@gmail.com>
2013-04-17 01:46:45 +0400

    MapReduceDriver is OK!

--- mapreduce-testing-mrjob/src/test/java/ru/develbureau/mrtesting/mapreduce/AccessLogMapReduceUnitTest.java
+++ mapreduce-testing-mrjob/src/test/java/ru/develbureau/mrtesting/mapreduce/AccessLogMapReduceUnitTest.java
@@ -4,6 +4,7 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.develbureau.mrtesting.model.LoggedRequest;
 import ru.develbureau.mrtesting.parser.ApacheLogParser;
@@ -17,22 +18,34 @@ import ru.develbureau.mrtesting.parser.ParserException;
 public class AccessLogMapReduceUnitTest {
     private final ApacheLogParser parser = new ApacheLogParser();
 
-    @Test
-    public void runMapReduceWithCombiner() throws ParserException {
-        MapReduceDriver<LongWritable, Text, LoggedRequest, LongWritable, NullWritable, Text>  mapReduceDriver = new MapReduceDriver();
+    private MapReduceDriver<LongWritable, Text, LoggedRequest, LongWritable, NullWritable, Text>  mapReduceDriver;
+    private final AccessLogReducer accessLogReducer = new AccessLogReducer();
+
+    @BeforeMethod
+    public void prepareDriver(){
+        System.out.println("prepareDriver");
+
+        mapReduceDriver = new MapReduceDriver();
         mapReduceDriver.setMapper(new AccessLogParserMapper());
         mapReduceDriver.setCombiner(new AccessLogCombiner());
-        AccessLogReducer accessLogReducer = new AccessLogReducer();
         mapReduceDriver.setReducer(accessLogReducer);
+
+    }
+    @Test
+    public void runMapReduceWithCombinerForShuttle() throws ParserException {
 
         mapReduceDriver.withInput(new LongWritable(), new Text("205.189.154.54 - - [01/Jul/1995:00:00:29 -0400] \"GET /shuttle/countdown/count.gif HTTP/1.0\" 200 40310"))
                        .withInput(new LongWritable(), new Text("205.189.155.66 - - [01/Jul/1995:00:01:15 -0400] \"GET /shuttle/countdown/count.gif HTTP/1.0\" 200 40310"))
-
-                       //.withInput(new LongWritable(), new Text("210.189.154.54 - - [05/Jun/1995:02:01:15 -0400] \"GET /moon/landing HTTP/1.0\" 200 40310"))
-                       //.withInput(new LongWritable(), new Text("212.189.154.54 - - [05/Jun/1995:06:01:15 -0400] \"GET /moon/landing HTTP/1.0\" 200 40310"))
-
-                       .withOutput(NullWritable.get(), accessLogReducer.prettyPrint(new LoggedRequest(parser.parseTimestamp("05/Jun/1995:02:01:15 -0400"), "/shuttle/countdown/count.gif", 200), 2))
-                       //.withOutput(NullWritable.get(), accessLogReducer.prettyPrint(new LoggedRequest(parser.parseTimestamp("05/Jun/1995:02:01:15 -0400"), "/moon/landing", 200), 2))
+                       .withOutput(NullWritable.get(), accessLogReducer.prettyPrint(new LoggedRequest(parser.parseTimestamp("05/Jun/1995:02:01:15 -0400"), "/moon/landing", 200), 1))
                        .runTest();
+    }
+
+    @Test
+    public void runMapReduceWithCombinerForMoon() throws ParserException {
+        mapReduceDriver.withInput(new LongWritable(), new Text("210.189.154.54 - - [05/Jun/1995:02:55:15 -0400] \"GET /moon/landing HTTP/1.0\" 200 40310"))
+                       .withInput(new LongWritable(), new Text("212.189.154.54 - - [05/Jun/1995:02:01:15 -0400] \"GET /moon/landing HTTP/1.0\" 200 40310"))
+                .withOutput(NullWritable.get(), accessLogReducer.prettyPrint(new LoggedRequest(parser.parseTimestamp("05/Jun/1995:02:01:15 -0400"), "moon/landing", 200), 2))
+                .runTest();
+
     }
 }
